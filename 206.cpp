f922b501d4bcee48bb11499c4f2427635e7446cc
zhueh <zhueh1991@sina.com>
2016-07-11 22:54:31 +0800

    add recursively

--- 206.cpp
+++ 206.cpp
@@ -29,4 +29,24 @@ public:
 };
 
 //recursively
-
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		if (head == NULL || head->next == NULL){
+			return head;
+		}
+		ListNode* nextNode = head->next;
+		ListNode* res = reverseList(head->next);
+		nextNode->next = head;
+		head->next = NULL;
+		return res;
+	}
+};
